770ae0f34891df4a434c6d572ed1730df0f3ba0f
Luiz-gabrielsilva <kanekisupremovibe@gmail.com>
2022-08-06 22:07:08 -0300

    created delivery models

--- delivery/models.py
+++ delivery/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    class OrderStatus(models.TextChoices):
+        RECEIVED = 'RECEIVED', ('RECEIVED')
+        CONFIRMED = 'CONFIRMED', ('CONFIRMED')
+        DISPATCHED = 'DISPATCHED', ('DISPATCHED')
+        DELIVERED = 'DELIVERED', ('DELIVERD')
+        CANCELED = 'CANCELED', ('CANCELED')
+   
+    cliente = models.CharField(max_length=200)
+    produto = models.CharField(max_length=200)
+    valor = models.FloatField()
+    entregue = models.BooleanField()
+    estado = models.CharField(max_length=10, choices=OrderStatus.choices, blank=False, null=False, default='RECEIVED')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return self.produto
